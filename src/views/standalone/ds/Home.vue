246e5527032315ca84f86871c13fae96b6275de3
erdnando <erdnando@gmail.com>
2019-07-12 18:37:13 -0500

    busqueda

--- src/views/standalone/ds/Home.vue
+++ src/views/standalone/ds/Home.vue
@@ -93,7 +93,20 @@
         <template>
         <div>
           <v-toolbar color="indigo" dark tabs>
-            <v-text-field append-icon="search" class="mx-3" flat label="Search"  solo-inverted></v-text-field>
+            <!-- <v-text-field append-icon="search" class="mx-3" flat label="Search"  solo-inverted></v-text-field> -->
+
+           <template>
+            <v-combobox @keyup.enter="busqueda" style="margin-top: 10px;color:black" autofocus background-color="#8C9EFF"  v-model="chips" :items="elementos" label="Search" chips clearable append-icon="search" solo multiple>
+              <template v-slot:selection="data">
+                <v-chip color="green" :selected="data.selected" close @input="remove(data.item)">
+                  <strong>{{ data.item }}</strong>&nbsp;
+                </v-chip>
+              </template>
+            </v-combobox>
+          </template>
+
+
+
             <template v-slot:extension>
               <v-tabs v-model="tabs" centered color="transparent" slider-color="white">
                 <v-tab >
@@ -111,7 +124,57 @@
             <v-tab-item >
               <v-card>
                 <v-card-text>
-                  tab1
+                  <template>
+                      <v-card>
+                        <v-card-title class="indigo white--text headline">
+                          User Directory
+                        </v-card-title>
+                        <v-layout justify-space-between pa-3>
+                          <v-flex xs5>
+                            <v-treeview :active.sync="active" :items="items"
+                              :load-children="fetchFiles" :open.sync="open"
+                              activatable active-class="primary--text"
+                              class="grey lighten-5" open-on-click transition>
+                              <template v-slot:prepend="{ item, active }">
+                                <v-icon v-if="!item.children" :color="active ? 'primary' : ''">folder</v-icon>
+                              </template>
+                            </v-treeview>
+                          </v-flex>
+                          <v-flex d-flex text-xs-center>
+                            <v-scroll-y-transition mode="out-in">
+                              <div v-if="!selected" class="title grey--text text--lighten-1 font-weight-light" style="align-self: center;">
+                                Select a User
+                              </div>
+                              <v-card v-else :key="selected.id" class="pt-4 mx-auto" flat max-width="400">
+                                <v-card-text>
+                                  <v-avatar v-if="avatar" size="88">
+                                    <v-img :src="`https://avataaars.io/${avatar}`" class="mb-4"></v-img>
+                                  </v-avatar>
+                                  <h3 class="headline mb-2">{{ selected.name }}</h3>
+                                  <div class="blue--text mb-2">{{ selected.email }}</div>
+                                  <div class="blue--text subheading font-weight-bold">{{ selected.username }}</div>
+                                </v-card-text>
+                                <v-divider></v-divider>
+                                <v-layout tag="v-card-text" text-xs-left wrap>
+                                  <v-flex tag="strong" xs5 text-xs-right mr-3 mb-2>Company:</v-flex>
+                                  <v-flex>{{ selected.company.name }}</v-flex>
+                                  <v-flex tag="strong" xs5 text-xs-right mr-3 mb-2>Website:</v-flex>
+                                  <v-flex>
+                                    <a :href="`//${selected.website}`" target="_blank">{{ selected.website }}</a>
+                                  </v-flex>
+                                  <v-flex tag="strong" xs5 text-xs-right mr-3 mb-2>Phone:</v-flex>
+                                  <v-flex>{{ selected.phone }}</v-flex>
+                                </v-layout>
+                              </v-card>
+                            </v-scroll-y-transition>
+                          </v-flex>
+                        </v-layout>
+                      </v-card>
+                    </template>
+
+
+
+
                 </v-card-text>
               </v-card>
             </v-tab-item>
@@ -156,7 +219,20 @@
 </template>
 
 <script>
+
+ const avatars = [
+    '?accessoriesType=Blank&avatarStyle=Circle&clotheColor=PastelGreen&clotheType=ShirtScoopNeck&eyeType=Wink&eyebrowType=UnibrowNatural&facialHairColor=Black&facialHairType=MoustacheMagnum&hairColor=Platinum&mouthType=Concerned&skinColor=Tanned&topType=Turban',
+    '?accessoriesType=Sunglasses&avatarStyle=Circle&clotheColor=Gray02&clotheType=ShirtScoopNeck&eyeType=EyeRoll&eyebrowType=RaisedExcited&facialHairColor=Red&facialHairType=BeardMagestic&hairColor=Red&hatColor=White&mouthType=Twinkle&skinColor=DarkBrown&topType=LongHairBun',
+    '?accessoriesType=Prescription02&avatarStyle=Circle&clotheColor=Black&clotheType=ShirtVNeck&eyeType=Surprised&eyebrowType=Angry&facialHairColor=Blonde&facialHairType=Blank&hairColor=Blonde&hatColor=PastelOrange&mouthType=Smile&skinColor=Black&topType=LongHairNotTooLong',
+    '?accessoriesType=Round&avatarStyle=Circle&clotheColor=PastelOrange&clotheType=Overall&eyeType=Close&eyebrowType=AngryNatural&facialHairColor=Blonde&facialHairType=Blank&graphicType=Pizza&hairColor=Black&hatColor=PastelBlue&mouthType=Serious&skinColor=Light&topType=LongHairBigHair',
+    '?accessoriesType=Kurt&avatarStyle=Circle&clotheColor=Gray01&clotheType=BlazerShirt&eyeType=Surprised&eyebrowType=Default&facialHairColor=Red&facialHairType=Blank&graphicType=Selena&hairColor=Red&hatColor=Blue02&mouthType=Twinkle&skinColor=Pale&topType=LongHairCurly'
+  ]
+
+  const pause = ms => new Promise(resolve => setTimeout(resolve, ms))
+
 import {bus} from '../../../main.js'
+import axios from "axios";
+
 
    export default {
     data(){
@@ -169,7 +245,13 @@ import {bus} from '../../../main.js'
         colorNotificacion:'',
         botonDeshabilitado:false,
         tabs: null,
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        text: 'Lorem ipseiusmod tempor incididunt ut labore et dolore magna aliis nisi ut aliquip ex ea commodo consequat.',
+        active: [],
+        avatar: null,
+        open: [],
+        users: [],
+         chips: [],
+        elementos: []
       }
     },
     methods:{
@@ -183,6 +265,68 @@ import {bus} from '../../../main.js'
         this.dialog = false;
         // implementacion en navbar
         bus.$emit('loginApp', {"user":"admin","pwd":"12345","app":"Digital Docs"});
+      },
+       remove (item) {
+        this.chips.splice(this.chips.indexOf(item), 1)
+        this.chips = [...this.chips]
+      },
+      async consultaSolr(consulta){
+          axios({
+                method: "post",
+                url: 'https://sminet.com.mx/OneRingService/Service1.svc/postCall',
+                timeout: 1000 * 10, // Wait for 10 seconds
+                headers: {
+                  "Content-Type": "application/json"
+                },
+                data: {
+                  telefono: telefono
+                }
+              })
+                .then(response => {
+                   console.log("SID...");
+                   console.log(response.data.Sid);
+                  console.log("sleeping...");
+                  this.sleep(1500);
+                   console.log("wake up...intentarPedirOneRing intento 0");
+                  this.intentarPedirOneRing(response.data.Sid, tipoTelefono, 0);
+
+                })
+                .catch(error => {
+                  console.log(error);
+                  
+                  if(tipoTelefono==1){
+                        this.homePhoneStatus="invalido";
+                        this.loading=false;
+                        this.colorBoton='red white--text'
+                     }
+                     else if(tipoTelefono==2){
+                        this.cellPhoneStatus="invalido";
+                         this.loading2=false;
+                         this.colorBoton2='red white--text'
+                     }
+              });
+    },
+      async fetchFiles (item) {
+        // Remove in 6 months and say
+        // you've made optimizations! :)
+        await pause(800)
+
+        return fetch('https://jsonplaceholder.typicode.com/users')
+          .then(res => res.json())
+          .then(json => (item.children.push(...json)))
+          .catch(err => console.warn(err))
+      },
+      randomAvatar () {
+        this.avatar = avatars[Math.floor(Math.random() * avatars.length)]
+      },
+      busqueda(){
+        console.log("buscando...");
+       // console.log(this.chips);
+        var strConsulta='';
+        for(var i=0;i<this.chips.length;i++){
+            strConsulta+= this.chips[i]+'-';
+        }
+        console.log(strConsulta);
       }
     },
     created(){
@@ -203,8 +347,26 @@ import {bus} from '../../../main.js'
     computed:{
          section1 () {
        return require('../../../assets/digital.jpg')
-       }
-    }
+       },
+       items () {
+        return [
+          {
+            name: 'Results',
+            children: this.users
+          }
+        ]
+      },
+      selected () {
+        if (!this.active.length) return undefined
+
+        const id = this.active[0]
+
+        return this.users.find(user => user.id === id)
+      }
+    },
+     watch: {
+      selected: 'randomAvatar'
+    },
     
     
   }
